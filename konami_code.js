938f1fb00d29769e15a87e41fb3720857a3c46af
Rachel Wyatt <rw126@evansville.edu>
2019-04-20 21:12:44 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,5 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
+let index = 0
 document.addEventListener("keydown", function(e) {
   init(e)
 //  alert(3)
@@ -7,7 +7,6 @@ document.addEventListener("keydown", function(e) {
 
 function init(e) {
   // Write your JavaScript code inside the init() function
-  let index = 0;
   const key = parseInt(e.detail || e.which);
 
   if (key === code[index]) {
