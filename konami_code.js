7be2756d05fd8281e8ca694648ecdd6e79d4f821
Rachel Wyatt <rw126@evansville.edu>
2019-04-20 21:09:22 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -4,13 +4,13 @@ function init() {
   // Write your JavaScript code inside the init() function
   let index = 0;
   const key = parseInt(e.detail || e.which);
- 
+
   if (key === code[index]) {
     index++;
- 
+
     if (index === code.length) {
       alert("Hurray!");
- 
+
       index = 0;
     }
   } else {
